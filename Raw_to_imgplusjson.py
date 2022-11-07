cf0c118d2aeeb02ef65e86fa37f8a3a06ef19a99
alexyhzou <alexyhzou@qq.com>
2022-11-07 13:42:37 +0800

    完成数据初步处理

--- Raw_to_imgplusjson.py
+++ Raw_to_imgplusjson.py
@@ -2,21 +2,23 @@
 time:   2022.11.4
 func:   将原始的数据变为单一文件夹下的数据。
         命名方法：原始子文件夹+_+照片+.jpeg/.json
+        需要注意两个地址
 """
 
 import os
 import shutil
 
+# create datasets folder if not have
 datasets = r"D:\learning\UNNC 科研\Cervical\datasets"
 if not os.path.exists(datasets):
     os.mkdir(datasets)
-    # 123
-    # 456dev2
+
 # global
 raw_data_dir = r"D:\learning\UNNC 科研\202210_CSD超声标注_图片及视频\CSD图片及标注_170例_507张"
 target = r"D:\learning\UNNC 科研\Cervical\datasets\Cervical_data"
 if not os.path.exists(target):
     os.mkdir(target)
+
 
 def main():
     for root, dirs, files in os.walk(raw_data_dir):
@@ -28,11 +30,11 @@ def main():
         #     print(os.path.join(root, f))
         # 遍历所有的文件夹
         for d in dirs:
-            cur_dir=os.path.join(root, d)
+            cur_dir = os.path.join(root, d)
             for cur_file in os.listdir(cur_dir):
-                cur_source=os.path.join(cur_dir,cur_file)
-                cur_target=os.path.join(target,d+'_'+cur_file)
-                shutil.copy(cur_source,cur_target)
+                cur_source = os.path.join(cur_dir, cur_file)
+                cur_target = os.path.join(target, d + '_' + cur_file)
+                shutil.copy(cur_source, cur_target)
 
 
 if __name__ == '__main__':
