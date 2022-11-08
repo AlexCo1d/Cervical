591b12610e95174ea3ddbf894e31a46579e55d05
Administrator <2242062912@qq.com>
2022-11-08 20:47:27 +0800

    完成负样本数据处理方法

--- preprocess_negative_sample.py
+++ preprocess_negative_sample.py
@@ -5,8 +5,9 @@ func:   将datasets数据中的Cervical_data中的负样本进行更新
 """
 
 import os
+import cv2
 import shutil
-from labelme import utils
+import numpy as np
 
 # global
 source = "datasets/Cervical_data"
@@ -15,7 +16,24 @@ SegmentationClass = "datasets/SegmentationClass"
 
 if __name__ == '__main__':
     for cur in os.listdir(source):
-        cur= cur.replace(".jpg",".json") if cur.endswith(".jpg") else( cur.replace(".bmp",".json") if cur.endswith(".bmp") else())
-        cur_dir=os.path.join(source,cur)
-        if not os.path.isfile(cur_dir):
-            shutil.copy(cur_dir, os.path.join(JPEGImages,cur))
+
+        # 仅处理jpg或者bmp文件
+        if cur.endswith(".jpg") or cur.endswith(".bmp"):
+            image = cv2.imread(os.path.join(source, cur), -1)
+            # 替换成json进行检测
+            cur_json = ""
+            if cur.endswith(".jpg"):
+                cur_json = cur.replace(".jpg", ".json")
+            elif cur.endswith(".bmp"):
+                cur_json = cur.replace(".bmp", ".json")
+            cur_json_dir = os.path.join(source, cur_json)
+            cur_dir = os.path.join(source, cur)
+
+            # 如果没有该json文件，转化成jpg到JPEGImages中，并且生成一个空白的mask到SegmentationClass中
+            if not os.path.isfile(cur_json_dir):
+                if cur.endswith(".jpg"):
+                    shutil.copy(cur_dir, os.path.join(JPEGImages, cur))
+                else:
+                    cv2.imwrite(os.path.join(JPEGImages, cur.replace(".bmp", ".jpg")), image)
+                # 添加mask
+                cv2.imwrite(os.path.join(SegmentationClass, cur[:-4] + ".png"), np.zeros(image.shape[:2]))
